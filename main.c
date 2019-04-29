1eae20f6f83788d5594ae1de462bb5dc0cc29995
Riley Arentz <c3328269@uon.edu.au>
2019-04-29 00:56:01 +0000

    much improvement, havent committed before hand

--- main.c
+++ main.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
-//rotation cipher, asii key is below, first steps.
+
+void caesar (char cipher[], int shift);
+
 int main() {
-    char A=65, B=66, C=67, D=68, E=69, F=70, G=71, H=72, I=73, J=74, K=75, L=76, M=77, N=78, O=79, P=80, Q=81, R=82, S=83, T=84, U=85, V=86, W=87, X=88, Y=89, Z=90;
-    char a=97, b=98, c=99, d=100, e=101, f=102, g=103, h=104, i=105, j=106, k=107, l=108, m=109, n=110, o=111, p=112, q=113, r=114, s=115, t=116, u=117, v=118, w=119, x=120, y=121, z=122;
+    
+    char message[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char 
     int key;
-    int encrypt;
     
     
 printf("Enter a message to encrypt: ");
-scanf("%d\n", &char);
+scanf("%d\n", char);
+
 printf("enter a key: ");
 scanf("%d\n", &key);
 
-encrypt=(char + key)*(mod 26);
-printf("%d\n", &encrypt);
+
 
 
 return 0;
+}
+
+void caesar (char cipher[], int shift) {
+    int = 0;
+    
+    while (cipher[i] != '\0'){
+        if((cipher[i] + shift) >= 65 && (cipher[i] + shift) <=90) {
+            cipher[i] += (shift);
+        } else {
+            cipher[i] += (shift - ('Z' - 'A'));
+        }
+        i++;
+    }
+    printf("%s", cipher);
 }
